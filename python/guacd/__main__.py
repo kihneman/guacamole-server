da5682fa044faabca1d59152d1ac4eb702ffe891
Kevin Kihneman <kihneman@gmail.com>
2023-11-16 21:25:37 -0600

    Add Python wrapper

---
+++ python/guacd/__main__.py
@@ -0,0 +1,23 @@
+import ctypes
+import os
+import sys
+from os.path import exists, join
+
+
+GUACD_DLL = join(os.sep, 'opt', 'guacamole', 'lib', 'libguacd.so')
+
+
+def main():
+    if exists(GUACD_DLL):
+        guacd_lib = ctypes.CDLL('/opt/guacamole/lib/libguacd.so')
+        guacd_lib.main.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_char_p)]
+        guacd_lib.main.restype = ctypes.c_int
+        argc = ctypes.c_int(len(sys.argv))
+        argv = (ctypes.c_char_p * len(sys.argv))(ctypes.c_char_p(s.encode()) for s in sys.argv)
+        return guacd_lib.main(argc, argv)
+    else:
+        print(f'The Guacamole Server library {GUACD_DLL} does not exist.')
+        return 'Missing DLL'
+
+
+print(f'Running with result "{main()}"')
