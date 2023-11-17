b354e39052c53892f98d302c05194b2ebfc92d10
James Muehlner <james.muehlner@guac-dev.org>
2023-11-17 22:42:46 +0000

    Fix configure syntax error.

--- scripts/build-guac.sh
+++ scripts/build-guac.sh
@@ -8,9 +8,7 @@ export LDFLAGS="-L/quasi-msys2/root/ucrt64/bin/"
 export CFLAGS="-I/quasi-msys2/root/ucrt64/include/ -I/usr/x86_64-w64-mingw32/include"
 
 autoreconf -fi
-./configure --host=x86_64-w64-mingw32 --with-cygwin --with-telnet=no --with-ssh=no --with-rdp=no --with-vnc=no --disable-kubernetes --disable-guacenc --disable-guacd --disable-guaclog
-
-cat config.log
+./configure --host=x86_64-w64-mingw32 --with-cygwin --with-telnet=no --with-ssh=no --with-rdp=no --with-vnc=no --disable-kubernetes --disable-guacenc --disable-guacd --disable-guaclog || cat config.log
 
 make
 make install
