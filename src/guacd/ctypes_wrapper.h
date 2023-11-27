e8f5c5526cf0e2c414cb4092d8e0c8d646ab2bad
Kevin Kihneman <kihneman@gmail.com>
2023-11-27 13:47:34 -0600

    Add more config functions to guacd ctypes wrapper

--- src/guacd/ctypes_wrapper.h
+++ src/guacd/ctypes_wrapper.h
@@ -7,7 +7,93 @@
 
 #include <guacamole/client-types.h>
 
-int main(int argc, char* argv[]);
+/** conf.h **/
+/**
+ * The default host that guacd should bind to, if no other host is explicitly
+ * specified.
+ */
+#define GUACD_DEFAULT_BIND_HOST "localhost"
+
+/**
+ * The default port that guacd should bind to, if no other port is explicitly
+ * specified.
+ */
+#define GUACD_DEFAULT_BIND_PORT "4822"
+
+/**
+ * The contents of a guacd configuration file.
+ */
+typedef struct guacd_config {
+
+    /**
+     * The host to bind on.
+     */
+    char* bind_host;
+
+    /**
+     * The port to bind on.
+     */
+    char* bind_port;
+
+    /**
+     * The file to write the PID in, if any.
+     */
+    char* pidfile;
+
+    /**
+     * Whether guacd should run in the foreground.
+     */
+    int foreground;
+
+    /**
+     * Whether guacd should simply print its version information and exit.
+     */
+    int print_version;
+
+#ifdef ENABLE_SSL
+    /**
+     * SSL certificate file.
+     */
+    char* cert_file;
+
+    /**
+     * SSL private key file.
+     */
+    char* key_file;
+#endif
+
+    /**
+     * The maximum log level to be logged by guacd.
+     */
+    guac_client_log_level max_log_level;
+
+} guacd_config;
+
+
+/** conf-file.h **/
+/**
+ * Loads the configuration from any of several default locations, if found. If
+ * parsing fails, NULL is returned, and an error message is printed to stderr.
+ */
+guacd_config* guacd_conf_load();
+
+
+/** conf-args.h **/
+/**
+ * Parses the given arguments into the given configuration. Zero is returned on
+ * success, and non-zero is returned if arguments cannot be parsed.
+ */
+int guacd_conf_parse_args(guacd_config* config, int argc, char** argv);
+
+
+/** log.h **/
+/**
+ * Writes a message to guacd's logs. This function accepts parameters
+ * identically to printf.
+ */
 void guacd_log(guac_client_log_level level, const char* format, ...);
+
+
+int main(int argc, char* argv[]);
 
 #endif
