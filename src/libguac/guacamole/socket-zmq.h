743ffb5bd1b17e151f4225f43ec046d9e45dc6e7
Kevin Kihneman <kihneman@gmail.com>
2024-01-05 14:18:00 -0600

    Add guac_socket_create_zmq to the header

--- src/libguac/guacamole/socket-zmq.h
+++ src/libguac/guacamole/socket-zmq.h
@@ -17,7 +17,7 @@
 #include <czmq.h>
 
 /**
- * Creates a new guac_socket which will use the CZMQ C binding of ZeroMQ
+ * Opens a new guac_socket which will use the CZMQ C binding of ZeroMQ
  * for all communication. Freeing this guac_socket will
  * automatically close the associated ZeroMQ socket handle.
  *
@@ -30,5 +30,40 @@
  *     for all communication.
  */
 guac_socket* guac_socket_open_zmq(zsock_t zsock);
+
+/**
+ * Creates a new guac_socket which will use the CZMQ C binding of ZeroMQ
+ * for all communication. Freeing this guac_socket will
+ * automatically close the associated ZeroMQ socket handle.
+ *
+ * @param type
+ *     The ZeroMQ socket type integer based on the socket type as follows:
+ *         ZMQ_PAIR 0
+ *         ZMQ_PUB 1
+ *         ZMQ_SUB 2
+ *         ZMQ_REQ 3
+ *         ZMQ_REP 4
+ *         ZMQ_DEALER 5
+ *         ZMQ_ROUTER 6
+ *         ZMQ_PULL 7
+ *         ZMQ_PUSH 8
+ *         ZMQ_XPUB 9
+ *         ZMQ_XSUB 10
+ *         ZMQ_STREAM 11
+ *
+ * @param endpoints
+ *     The endpoints pointer is NULL, or points to string starting with
+ *     '@' (bind) or '>' (connect). Multiple endpoints are allowed, separated by
+ *     commas. If the endpoint does not start with '@' or '>', the serverish argument defines
+ *     whether it is used to bind (serverish = true) or connect (serverish = false).
+ *
+ * @param serverish
+ *     The serverish argument defines whether it is used to bind (serverish = true) or connect (serverish = false).
+ *
+ * @return
+ *     A newly-allocated guac_socket which will transparently use the CZMQ C binding of ZeroMQ
+ *     for all communication.
+ */
+guac_socket* guac_socket_create_zmq(int type, const char *endpoints, bool serverish);
 
 #endif
