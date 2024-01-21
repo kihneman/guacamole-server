a1917a3cb82f5a8b9b2b748b8d5c0ffab4a7909c
Kevin Kihneman <kihneman@gmail.com>
2024-01-20 23:45:19 -0600

    Add daemon.main

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'guacd=guacd.__main__:main'
+            'guacd=guacd.daemon:main'
         ]
     }
 )
