08c18f66cdf570f53f16a328c0e358f77a704df6
James Muehlner <james.muehlner@guac-dev.org>
2024-09-30 18:46:11 +0000

    Merge GUACAMOLE-1841-mingw-build-clean-xx97.

--- src/terminal/terminal.c
+++ src/terminal/terminal.c
@@ -49,6 +49,12 @@
 #include <guacamole/socket.h>
 #include <guacamole/string.h>
 #include <guacamole/timestamp.h>
+#include <guacamole/user.h>
+
+#ifdef WINDOWS_BUILD
+#include "terminal/wcwidth.h"
+#include <guacamole/pipe.h>
+#endif
 
 /**
  * Sets the given range of columns to the given character.
