9a009d68563fae349f0ebdf3d32d592a0aa2d905
Kevin Kihneman <kkihneman@keepersecurity.com>
2025-08-04 14:25:13 -0500

    Merge GUACAMOLE-1841-mingw-build-clean-xx94.

--- src/terminal/display.c
+++ src/terminal/display.c
@@ -26,6 +26,10 @@
 #include "terminal/terminal-priv.h"
 #include "terminal/types.h"
 
+#ifdef WINDOWS_BUILD
+#include <terminal/wcwidth.h>
+#endif
+
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
